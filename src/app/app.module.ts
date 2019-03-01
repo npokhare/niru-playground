cac9d92910779002546b2ee938213cc6d44c670e
xeian_sangeet@yahoo.com <C58562@launcher.int.abnamro.com>
2019-03-01 17:59:30 +0100

    add the cool stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroImageComponent } from './hero-image/hero-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    FooterComponent,
+    HeroImageComponent
   ],
   imports: [
     BrowserModule,
